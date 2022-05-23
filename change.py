441f337dea08f4323176ba90af2885fde268e1f7
CodeWithMe2001 <94504035+CodeWithMe2001@users.noreply.github.com>
2022-05-23 11:15:42 -0500

    Add files via upload

---
+++ change.py
@@ -0,0 +1,24 @@
+import os
+import fileinput
+
+files = []
+
+for x in os.listdir():
+    if x == "main.py" or x == "chars.txt":
+        continue
+
+    if os.path.isfile(x):
+        files.append(x)
+
+looking = True
+while looking:
+    text = input("Please input the text you would like to change: Enter 'exit' to quit: ")
+    new = input("Please input the character you would like to replace with: ")
+
+    for x in files:
+        file = fileinput.FileInput(x, inplace=True, backup=False)
+        for line in file:
+            print(line.replace(text, new), end='')
+
+    if text == "exit":
+        looking = False
